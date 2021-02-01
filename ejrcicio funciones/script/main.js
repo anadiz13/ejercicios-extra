19246363fc3b73c3c05c646465969de6abc95d0e
ana diz <ana_diz137@hotmail.es>
2021-02-01 11:59:58 +0100

    add exercise 2

--- ejrcicio funciones/script/main.js
+++ ejrcicio funciones/script/main.js
@@ -1,6 +1,17 @@
 `use strict`;
-function ten() {}
-let numberSelected = "papata";
-for (let i = 0; i < 11; i++) {
-  console.log("patata" + i);
+
+function ten() {
+  for (let i = 0; i < 11; i++) {
+    console.log("patata");
+  }
 }
+ten();
+
+function ten(word) {
+  for (let i = 0; i < numero; i++) {
+    console.log(word);
+  }
+}
+ten("Patata"); //ten(word="patata", numero=10)
+ten("Aguacate");
+ten("Pizza");
