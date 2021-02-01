9363f2343293028b5d154dc9ff34a20296e46570
ana diz <ana_diz137@hotmail.es>
2021-02-01 12:04:39 +0100

    add exercise 2

--- ejrcicio funciones/script/main.js
+++ ejrcicio funciones/script/main.js
@@ -15,3 +15,12 @@ function ten(word) {
 ten("Patata"); //ten(word="patata", numero=10)
 ten("Aguacate");
 ten("Pizza");
+
+function ten(word, numero) {
+  for (let i = 0; i < numero; i++) {
+    console.log(word);
+  }
+}
+ten("Patata", 10); //ten(word="patata", numero=10)
+ten("Aguacate", 07);
+ten("Pizza", 50);
